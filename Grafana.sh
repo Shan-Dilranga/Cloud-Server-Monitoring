021bf57140fe99b5411d0595f900b98c06d3670d
Shan Dilranga <84151287+Shan-Dilranga@users.noreply.github.com>
2025-08-19 15:19:16 +0530

    Installation script for Grafana

---
+++ Grafana.sh
@@ -0,0 +1,14 @@
+# Add Grafana APT repo
+sudo apt-get install -y software-properties-common
+sudo add-apt-repository "deb https://packages.grafana.com/oss/deb stable main"
+
+# Add Grafana GPG key
+wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
+
+# Update & install
+sudo apt update
+sudo apt install grafana -y
+
+#Start Grafana
+sudo systemctl enable grafana-server
+sudo systemctl start grafana-server
